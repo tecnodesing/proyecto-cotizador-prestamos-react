2f6334a033d6a2a0831ff668578df843f052528f
tecnodesing <snors21@gmail.com>
2023-04-05 19:49:04 -0600

    se creo componente HEADER

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Header from "./components/Header"
 
 function App() {
-  const hola = "hola mundo";
+  
 
   return (
     
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-        {hola}
+        <Header />  
       </div>
    
   )
