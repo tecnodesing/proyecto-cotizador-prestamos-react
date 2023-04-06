ab3b4ad3f81b2997cdf0c7a76528a8a7424980a4
tecnodesing <snors21@gmail.com>
2023-04-05 19:27:09 -0600

    se creó base de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  const hola = "hola mundo";
+
+  return (
+    
+      <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        {hola}
+      </div>
+   
+  )
+}
+
+export default App
